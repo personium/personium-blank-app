4b3a626523e38f38afff8bebc16715c01fbe3eeb
ishiguro-yo <ishiguro.yo@jp.fujitsu.com>
2019-07-11 18:00:59 +0900

    change to use babel7

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,15 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }],
       },
     ],
   },
