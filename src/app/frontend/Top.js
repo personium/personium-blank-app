c6c3f1bf331f1890002ac958d7b74f02e97feac8
ishiguro-yo <yoh1496@gmail.com>
2020-06-25 19:02:23 +0900

    Append dummy auth context and dummy page

--- src/app/frontend/Top.js
+++ src/app/frontend/Top.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Top() {
-  return <h1>Top Page</h1>;
+  return (
+    <div>
+      <h1>Top Page</h1>
+      <Link to="/user">UserPage</Link>
+    </div>
+  );
 }
